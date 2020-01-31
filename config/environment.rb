a27ccc2914a363cef94f534a8bf77fbfd54113b8
Barry Ivan Silber <age.ef.ess@gmail.com>
2020-01-31 13:32:03 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,9 @@ ENV['SINATRA_ENV'] ||= "development"
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
+  #:database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
+
+  :database => "db/development.sqlite"
 )
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
